12d25ab5c0f821a91cf5f3efd92918071b133476
Greg <brewer0131@gmail.com>
2021-07-12 22:54:07 -0500

    Update HeliumNetdataCollector.py

--- HeliumNetdataCollector.py
+++ HeliumNetdataCollector.py
@@ -1,63 +1,105 @@
+mport json
+
 from bases.FrameworkServices.UrlService import UrlService
-from jsonrpcclient.clients.http_client import HTTPClient
-from jsonrpcclient.exceptions import ReceivedErrorResponseError
-from jsonrpcclient.requests import Request
 
-
-
-	
-
-NETDATA_UPDATE_EVERY=1
-priority = 90000
-
+update_every = 5
 
 ORDER = [
-    "block_height"
+    'blockheight',
+    'blockage',
 ]
 
 CHARTS = {
-    "block_height_chart": {
-        'options': ["Block_Height", "blocks", "blocks", "Validator", "Current_Block_Height", "line"],
-        "lines": [
-            ["block_height","block"]
+    'blockheight': {
+        'options': [None, 'Validator Height', 'count', 'blockheight', 'miner_blockheight', 'area'],
+        'lines': [
+            ['blockchain_blocks', 'blocks', 'absolute'],
+            ['blockchain_headers', 'headers', 'absolute'],
         ]
-     }
+    },
+    'blockage': {
+        'options': [None, 'Blockchain Difficulty', 'difficulty', 'blockchain', 'energi.difficulty', 'line'],
+        'lines': [
+            ['blockchain_difficulty', 'Diff', 'absolute'],
+        ],
+    },
+    
 }
 
+METHODS = {
+    'getblockheightinfo': lambda r: {
+        'blockchain_blocks': r['blocks'],
+        'blockchain_headers': r['headers'],
+        'blockchain_difficulty': r['difficulty'],
+    },
+    'getmempoolinfo': lambda r: {
+        'mempool_txcount': r['size'],
+        'mempool_txsize': r['bytes'],
+        'mempool_current': r['usage'],
+        'mempool_max': r['maxmempool'],
+    },
+    'getmemoryinfo': lambda r: dict([
+        ('secmem_' + k, v) for (k, v) in r['locked'].items()
+    ]),
+    'getnetworkinfo': lambda r: {
+        'network_timeoffset': r['timeoffset'],
+        'network_connections': r['connections'],
+    },
+    'gettxoutsetinfo': lambda r: {
+        'utxo_count': r['txouts'],
+        'utxo_xfers': r['transactions'],
+        'utxo_size': r['disk_size'],
+        'utxo_amount': r['total_amount'],
+    },
+}
+
+JSON_RPC_VERSION = '2.0'
+
+
 class Service(UrlService):
-    def __init__(self, scheme="http", host="localhost", port=4467, configuration=None, name=None):
+    def __init__(self, configuration=None, name=None):
         UrlService.__init__(self, configuration=configuration, name=name)
         self.order = ORDER
         self.definitions = CHARTS
-        self.url = f'{scheme}://{host}:{port}/jsonrpc'
-       
-            
-    def block_height(self):
-	height = self.http_post("block_height")["height"]		  
-        return height
-
-    def logMe(self,msg):
-	self.debug(msg)  
+        self.host = self.configuration.get('host', '127.0.0.1')
+        self.port = self.configuration.get('port', 4467)
+        self.url = '{scheme}://{host}:{port}'.format(
+            scheme=self.configuration.get('scheme', 'http'),
+            host=self.host,
+            port=self.port,
+        )
+        self.method = 'POST'
+        self.header = {
+            'Content-Type': 'application/json',
+        }
 
     def _get_data(self):
-        data = dict()
+        #
+        # Helium Miner speaks JSON-RPC version 2.0 for maximum compatibility
+        #
+        # 1.0 spec: https://www.jsonrpc.org/specification_v1
+        # 2.0 spec: https://www.jsonrpc.org/specification
+        #
+        # The call documentation: ## https://github.com/helium/miner/tree/mra/jsonrpc/src/jsonrpc
+        #
+        batch = []
 
-        validatorstats = self.get_validator_stats()
-        if not validatorstats:
+        for i, method in enumerate(METHODS):
+            batch.append({
+                'version': JSON_RPC_VERSION,
+                'id': i,
+                'method': method,
+                'params': [],
+            })
+
+        result = self._get_raw_data(body=json.dumps(batch))
+
+        if not result:
             return None
 
-        data.update(validatorstats)
-	
-    def get_validator_stats
-	url = '{0}{1}'.format(self.url, '[blo
-	raw = self._get_raw_data(url)
-	
+        result = json.loads(result.decode('utf-8'))
         data = dict()
-            
-        return data
 
-
-
-
-
-
+        for i, (_, handler) in enumerate(METHODS.items()):
+            r = result[i]
+            data.update(handler(r['result']))
